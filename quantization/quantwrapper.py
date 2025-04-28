d21e83a8f63168dfab849fc121bfb1176b3b36ce
kyohmin <71572502+kyohmin@users.noreply.github.com>
2025-04-28 10:04:37 +0900

    Minor Update

--- quantization/quantwrapper.py
+++ quantization/quantwrapper.py
@@ -2,7 +2,42 @@ import torch
 import torch.nn as nn
 import torch.nn.functional as F
 
-from quantization.quantization import Quantization
+class Quantization:
+    @staticmethod
+    def quantize(original_tensor, quantization_mode = "asymmetric", range_estimator_type = "min_max", bits=8, zero = None, scale = None):
+        if quantization_mode =="asymmetric": min_q, max_q, dtype = 0, 2**bits - 1, torch.uint8
+        elif quantization_mode =="symmetric": min_q, max_q, dtype = -(2**(bits-1) - 1), (2**(bits-1) - 1), torch.int8
+
+        # Activation Quantization (scale and zero already given)
+        if zero is not None and scale is not None:
+            quantized_tensor =  torch.clamp(torch.round(original_tensor / scale + zero), min_q, max_q).to(dtype)
+            return quantized_tensor, scale, zero, dtype
+
+        # Set Range Estimator type
+        if range_estimator_type == "min_max":
+            min_v = original_tensor.min().item()
+            max_v = original_tensor.max().item()
+
+        # Find zero and scale value
+        if quantization_mode == "asymmetric":
+            span = max_v - min_v
+            scale = span / (max_q - min_q) if span != 0 else 1.0
+            zero = round(min_q - min_v / scale)
+            zero = int(max(min_q, min(max_q, zero)))
+
+        elif quantization_mode == "symmetric":
+            S = max(abs(min_v), abs(max_v))
+            scale = S / max_q if S != 0 else 1.0
+            zero = 0
+
+        quantized_tensor = torch.round(original_tensor / scale + zero)
+        quantized_tensor = torch.clamp(quantized_tensor, min_q, max_q).to(dtype)
+        return quantized_tensor, scale, zero, dtype
+
+    @staticmethod
+    def dequantize(quantized_tensor, zero, scale):
+        dequantized_tensor = (quantized_tensor.float() - zero) * scale
+        return dequantized_tensor
 
 class QuantWrapper(nn.Module):
     def __init__(self, module, weight=None, weight_scale=None, weight_zero=None, activation_zero=None, activation_scale=None):
@@ -11,8 +46,6 @@ class QuantWrapper(nn.Module):
         self.use_weightquant = False
         self.use_activationquant = True
         self.failed = False
-
-        # Remove when optimizing model
         self.dict = {"quantization_mode":"asymmetric", "range_estimator_type":"min_max", "bits":8}
 
         # Weight Quant Parameters
@@ -45,43 +78,40 @@ class QuantWrapper(nn.Module):
             self._update_activation_params(scale, zero)
 
             # Bit Shift
-            shift_bits = 4
+            shift_bits = 4 # 4 bit shift to fit the range for fp16
             quantized_activation = (quantized_activation.to(torch.float32) - self.activation_zero) >> shift_bits
             quantized_weight = (self.weight.to(torch.float32) - self.weight_zero) >> shift_bits
 
-            # quantized_activation = quantized_activation * shift_scale
-            # quantized_weight = quantized_weight * shift_scale
+            # Conv2d 
             if isinstance(self.module, nn.Conv2d):
-                output_16 = F.conv2d(quantized_activation,quantized_weight, bias=None, stride=self.module.stride, padding=self.module.padding, dilation=self.module.dilation, groups=self.module.groups)
+                output_16 = F.conv2d(quantized_activation, quantized_weight, bias=None, stride=self.module.stride, padding=self.module.padding, dilation=self.module.dilation, groups=self.module.groups) 
                 output_16 = output_16 * (self.weight_scale * self.activation_scale) * (1 << (2*shift_bits))
                 if self.module.bias is not None:
                     output_16 = output_16 + self.module.bias.view(1,-1,1,1)
 
+                # If failed, compute original conv2d
                 if torch.any(torch.isnan(output_16)) or torch.any(torch.isinf(output_16)):
-                    # print("Original - CONV2D")
                     self.failed = True
                     dequantized_weight = Quantization.dequantize(self.weight, self.weight_zero, self.weight_scale)
                     return F.conv2d(x, dequantized_weight, self.module.bias, self.module.stride, self.module.padding, self.module.dilation, self.module.groups)
-                # print("INT8 - CONV2D")
                 return output_16
-            
+
+            # Linear  
             elif isinstance(self.module, nn.Linear):
                 output_16 = F.linear(quantized_activation, quantized_weight, bias = None)
                 output_16 = output_16 * (self.weight_scale * self.activation_scale) * (1 << (2*shift_bits))
-
                 if self.module.bias is not None:
                     output_16 = output_16 + self.module.bias
 
+                # If failed, compute original linear
                 if torch.any(torch.isnan(output_16)) or torch.any(torch.isinf(output_16)):
-                    # print("Original - LINEAR")
                     self.failed = True
                     dequantized_weight = Quantization.dequantize(self.weight, self.weight_zero, self.weight_scale)
                     return F.linear(x, dequantized_weight, self.module.bias)
 
-                # print("INT8 - LINEAR")
                 return output_16
             
-        # Calculate INT8 -> FP32 x FP32
+        # For dequantize quantized wegiht for original computation
         elif self.use_weightquant and not self.use_activationquant:
             dequantized_weight = Quantization.dequantize(self.weight, self.weight_zero, self.weight_scale)
             if isinstance(self.module, nn.Conv2d):
@@ -91,7 +121,3 @@ class QuantWrapper(nn.Module):
 
         else:
             return self.module(x)
-
-
-
-
--- quantization/util.py
+++ quantization/util.py
@@ -1,10 +1,10 @@
 import torch
 
-from quantization.quantization import Quantization
+from quantization.quantwrapper import Quantization
 from quantization.quantwrapper import QuantWrapper
 
 class Util:
-    # MODEL WRAPPING =====
+    # MODEL WRAPPING
     @staticmethod
     def rewrap(model, layer_types=(torch.nn.Conv2d, torch.nn.Linear)):
         Util.add_wrapper(model, layer_types)
@@ -13,18 +13,15 @@ class Util:
     @staticmethod
     def add_wrapper(module, layer_types=(torch.nn.Conv2d, torch.nn.Linear), skip_keys=()):
         for name, child in module.named_children():
-            if any(skip_key in name for skip_key in skip_keys):
+            if any(skip_key in name for skip_key in skip_keys): # Layers to skip wrapping
                 continue
-            if isinstance(child, layer_types):
+            if isinstance(child, layer_types): # Wrap the selected layer
                 setattr(module, name, QuantWrapper(child))
             else:
-                # Recursively wrap inner children
-                Util.add_wrapper(child, layer_types)
+                Util.add_wrapper(child, layer_types) # Recursively go down
         return module
-
-        
     
-    # QUANTIZE WRAPPED MODEL =====
+    # QUANTIZE (WEIGHT) WRAPPED MODEL
     @staticmethod
     def quantize_model_weights(
         model,
@@ -33,7 +30,8 @@ class Util:
         calibration_type = "min_max",
         bits = 8
         ):
-        for name, module in model.model.diffusion_model.named_modules():
+        # If quantwrapper, quantize and update the weight
+        for _, module in model.model.diffusion_model.named_modules():
             if isinstance(module, QuantWrapper) and isinstance(module.module, layer_types):
                 with torch.no_grad():
                     if module.weight == None: weight = module.module.weight.data 
@@ -45,42 +43,6 @@ class Util:
                         del module.module._parameters['weight']
 
         return model
-    
-    # @staticmethod
-    # def quantize_model_weights(
-    #     model,
-    #     layer_types=(torch.nn.Linear, torch.nn.Conv2d),
-    #     uniform_type="asymmetric",
-    #     calibration_type="min_max",
-    #     bits=8
-    # ):
-    #     for name, module in model.model.diffusion_model.named_modules():
-    #         if isinstance(module, QuantWrapper) and isinstance(module.module, layer_types):
-    #             with torch.no_grad():
-    #                 # grab either the already‑stored quant weight or the raw float weight
-    #                 weight = module.weight if module.weight is not None else module.module.weight.data
-
-    #                 # use per‑channel quantization
-    #                 q_w, scales, zero_points, dtype = Quantization.quantize_by_channel(
-    #                     weight,
-    #                     uniform_type,
-    #                     calibration_type,
-    #                     bits
-    #                 )
-
-    #                 # update the wrapper with the new params
-    #                 module.update_weight_params(q_w, scales, zero_points, dtype)
-    #                 module.update_dict({
-    #                     "quantization_mode": uniform_type,
-    #                     "range_estimator_type": calibration_type,
-    #                     "bits": bits
-    #                 })
-
-    #                 # remove the original float parameter so only q_w remains
-    #                 if 'weight' in module.module._parameters:
-    #                     del module.module._parameters['weight']
-
-    #     return model
 
     # Activation Calibration
     @staticmethod
@@ -92,14 +54,13 @@ class Util:
         model,
         layer_types=(torch.nn.Linear, torch.nn.Conv2d),
     ):
-        from torch.nn import Module
 
         seen_ids = set()
         original, success, failed = 0, 0, 0
         linear_success, conv2d_success = 0, 0
         linear_failed, conv2d_failed = 0, 0
 
-        def scan(module: Module, prefix=""):
+        def scan(module, prefix=""):
             nonlocal original, success, failed
             nonlocal linear_success, conv2d_success, linear_failed, conv2d_failed
 
@@ -110,7 +71,7 @@ class Util:
                     continue
                 seen_ids.add(id(child))
 
-                # Case 1: QuantWrapper with target module
+                # Quantwrapper (Failed/Success) Count
                 if isinstance(child, QuantWrapper) and isinstance(child.module, layer_types):
                     if getattr(child, "failed", None) is False:
                         print(f"Success  - {full_name}", child.module)
@@ -129,17 +90,18 @@ class Util:
 
                     continue
 
-                # Case 2: Unwrapped Linear/Conv2d
+                # Original (Unwrapped) 
                 elif isinstance(child, layer_types):
                     print(f"Original - {full_name}", child)
                     original += 1
 
-                # Recurse into other children
+                # Recurse
                 else:
                     scan(child, prefix=full_name)
 
         scan(model.model.diffusion_model)
 
+        # Print Result
         print(f"Original (unwrapped): {original}")
         print(f"Quantized Success   : {success}")
         print(f"Quantized Failed    : {failed}")
